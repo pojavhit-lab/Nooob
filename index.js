6c53fb51ef9564664a51210ae369eee5ebe2f41e
pojavhit-lab <pojavhit@gmail.com>
2026-06-15 21:18:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
 const mineflayer = require('mineflayer')
 
-function createBot() {
-  const bot = mineflayer.createBot({
-    host: 'pojaverscore.falix.gg',
-    port: 43223,
-    username: 'AFK_Player',
-    version: '1.21.1'
-  })
+const bot = mineflayer.createBot({
+  host: 'pojaverscore.falix.gg',
+  port: 43223,
+  username: 'AFK_Player',
+  version: '1.21.1'
+})
 
-  bot.on('spawn', () => {
-    console.log('Bot joined!')
-  })
+bot.on('spawn', () => {
+  console.log('Bot joined!')
 
-  bot.on('end', () => {
-    console.log('Disconnected, reconnecting...')
-    setTimeout(createBot, 5000)
-  })
+  setTimeout(() => {
+    bot.chat('/register Password5758947 Password5758947')
+  }, 3000)
 
-  bot.on('error', console.log)
-}
-
-createBot()
-const http = require('http');
-
-http.createServer((req, res) => {
-  res.end('Bot Online');
-}).listen(process.env.PORT || 3000);
+  setTimeout(() => {
+    bot.chat('/login Password5758947')
+  }, 6000)
+})
