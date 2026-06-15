0f851bf0ce490d7aa4c6bdb269c2b848b0674077
pojavhit-lab <pojavhit@gmail.com>
2026-06-15 20:26:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const mineflayer = require('mineflayer');
 
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'YOUR_SERVER_IP',
+    port: 25565,
+    username: 'AFKBot'
+  });
+
+  bot.on('spawn', () => {
+    console.log('Bot joined!');
+  });
+
+  bot.on('end', () => {
+    console.log('Disconnected, reconnecting...');
+    setTimeout(createBot, 5000);
+  });
+
+  bot.on('error', err => console.log(err));
+}
+
+createBot();
