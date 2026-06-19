6d30a7009731cefa5e6f252a62448ed176f38b56
pojavhit-lab <pojavhit@gmail.com>
2026-06-19 18:48:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,15 @@ bot.on('spawn', () => {
     bot.chat('/login tyryjfyj688tr');
   }, 8000);
 });
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
