60ceb805425cc209951cbc8b42dc5e1b9eae3ec4
pojavhit-lab <pojavhit@gmail.com>
2026-06-19 18:39:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const mineflayer = require('mineflayer');
 
 const bot = mineflayer.createBot({
   host: 'pojaverscore.falix.gg',
-  port: 24439,
+  port: 25565,
   username: 'AFKKKK_BOT44444'
 });
 
 bot.on('spawn', () => {
-  console.log('Bot joined!');
-  
-  setInterval(() => {
-    bot.setControlState('jump', true);
-    setTimeout(() => bot.setControlState('jump', false), 500);
-  }, 30000);
+  setTimeout(() => {
+    bot.chat('/register tyryjfyj688tr tyryjfyj688tr');
+  }, 3000);
+
+  setTimeout(() => {
+    bot.chat('/login tyryjfyj688tr');
+  }, 8000);
 });
